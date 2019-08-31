292088eb4abd96d0c32ca099236f74941a4de34d
Bob Reselman <reselbob@gmail.com>
2019-08-30 17:51:34 -0700

    building out framework

---
+++ auction/messageBroker/index.js
@@ -0,0 +1,26 @@
+const {Publisher, Subscriber} = require('resel-redis-broker');
+
+const validateEnvVars = () => {
+    const missingEnvVars = [];
+    const envVarNames = getDependencyEnvVars();
+    envVarNames.forEach(envVar => {
+        if (!process.env[envVar]) missingEnvVars.push(envVar);
+    });
+
+    if (missingEnvVars.length > 0) {
+        const str = `The following required environment variable are missing: ${JSON.stringify(missingEnvVars)}. Server shutting down at ${new Date()}.`;
+        throw new Error(str);
+    }
+};
+
+
+const getDependencyEnvVar = (envVar) => {
+    return process.env[envVar];
+};
+
+const getDependencyEnvVars = () => {
+    return ['REDIS_HOST', 'REDIS_PORT',
+        'REDIS_PWD'];
+};
+
+module.exports = {validateEnvVars, getDependencyEnvVar, Publisher, Subscriber};
